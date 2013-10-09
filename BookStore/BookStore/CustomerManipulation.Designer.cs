1fbc1cc368a06482ffedb4196e640ebef812ad2e
Pawan <toshniwalpawan@gmail.com>
2013-10-09 22:21:10 +0530

    added new file

---
+++ BookStore/BookStore/CustomerManipulation.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace BookStore
+{
+    partial class CustomerManipulation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtPin = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.cmbName = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "City";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(65, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Pincode";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(137, 6);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(137, 62);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtAddress.TabIndex = 5;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(137, 103);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 6;
+            // 
+            // txtPin
+            // 
+            this.txtPin.Location = new System.Drawing.Point(137, 144);
+            this.txtPin.Name = "txtPin";
+            this.txtPin.Size = new System.Drawing.Size(100, 20);
+            this.txtPin.TabIndex = 7;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(68, 207);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(162, 207);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // cmbName
+            // 
+            this.cmbName.FormattingEnabled = true;
+            this.cmbName.Location = new System.Drawing.Point(137, 5);
+            this.cmbName.Name = "cmbName";
+            this.cmbName.Size = new System.Drawing.Size(121, 21);
+            this.cmbName.TabIndex = 10;
+            this.cmbName.Visible = false;
+            // 
+            // CustomerManipulation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.cmbName);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtPin);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CustomerManipulation";
+            this.Text = "CustomerManipulation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtPin;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ComboBox cmbName;
+    }
+}
---
+++ BookStore/BookStore/CustomerManipulation.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookStore
+{
+    public partial class CustomerManipulation : Form
+    {
+        public string strOperation = string.Empty; 
+        public CustomerManipulation()
+        {
+            InitializeComponent();
+        }
+
+        public CustomerManipulation(string op):this()
+        {
+             
+            strOperation = op;
+            if (strOperation == "A")
+            {
+                txtName.Visible = true;
+                cmbName.Visible = false; 
+
+            }
+            else
+            {
+                txtName.Visible = false;
+                cmbName.Visible = true;
+                DataSet ds = FillCustName();
+                cmbName.DataSource = ds.Tables[0];
+                cmbName.DisplayMember = "Name";
+                cmbName.ValueMember = "custid"; 
+
+            }
+            
+        }
+
+        private DataSet FillCustName()
+        {
+            try
+            {
+                string ConStr = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+                SqlConnection conn = new SqlConnection(ConStr);
+                SqlCommand com = new SqlCommand();
+                com.CommandType = CommandType.StoredProcedure;
+                com.Connection = conn;
+                com.CommandText = "CustomerList";
+                DataSet ds = new DataSet();
+                SqlDataAdapter adpater = new SqlDataAdapter("CustomerList", conn);
+                adpater.Fill(ds);
+                return ds;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string ConStr = ConfigurationManager.ConnectionStrings["conn"].ConnectionString; SqlConnection conn = new SqlConnection(ConStr);
+            SqlCommand com = new SqlCommand();
+            com.CommandType = CommandType.StoredProcedure;
+            com.Connection = conn;
+            com.CommandText = "Customermanipulation";
+            if(cmbName.SelectedValue != null) 
+                com.Parameters.Add(new SqlParameter("@custId",cmbName.SelectedValue));
+            else
+                com.Parameters.Add(new SqlParameter("@custId", 1));
+            com.Parameters.Add(new SqlParameter("@Name", txtName.Text));
+            com.Parameters.Add(new SqlParameter("@City", txtCity.Text));
+            com.Parameters.Add(new SqlParameter("@Address", txtAddress.Text));
+            com.Parameters.Add(new SqlParameter("@pincode", txtPin.Text));
+            com.Parameters.Add(new SqlParameter("@op", strOperation));
+            conn.Open(); 
+            int iResult = com.ExecuteNonQuery();
+            conn.Close();
+            if (strOperation == "A")
+                MessageBox.Show("Party added successfully");
+            else if (strOperation == "E")
+            {
+                MessageBox.Show("Party edited succesfully");
+            }
+            else
+            {
+                MessageBox.Show("Party deleted succesfully");
+            }
+            this.Hide(); 
+        }
+    }
+}
